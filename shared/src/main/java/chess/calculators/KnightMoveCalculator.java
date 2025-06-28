0d63fcf104a063998dc4fd19bd76d73694444c84
Howon Jung <jhwlds@gmail.com>
2025-06-27 21:28:06 -0600

    passed the knight move test

--- shared/src/main/java/chess/calculators/KnightMoveCalculator.java
+++ shared/src/main/java/chess/calculators/KnightMoveCalculator.java
@@ -1,7 +1,6 @@
 package chess.calculators;
 
 import chess.*;
-
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -15,22 +14,21 @@ public class KnightMoveCalculator implements PieceMovesCalculator {
         ChessGame.TeamColor myColor = currentPiece.getTeamColor();
 
         int[][] directions = {
-                {-1, -1},
-                {-1, 0},
-                {-1, 1},
-                { 0, -1},
-                { 0, 1},
-                { 1, -1},
-                { 1, 0},
-                { 1, 1}
+                {-2, -1},
+                {-2, +1},
+                {-1, -2},
+                {-1, +2},
+                {+1, -2},
+                {+1, +2},
+                {+2, -1},
+                {+2, +1}
         };
 
         for (int[] i : directions) {
             int row = position.getRow() + i[0];
             int col = position.getColumn() + i[1];
 
-            if (row < 1 || row > 8 || col < 1 || col > 8)
-                continue;
+            if (row < 1 || row > 8 || col < 1 || col > 8) continue;
 
             ChessPosition nextPosition = new ChessPosition(row, col);
             ChessPiece target = board.getPiece(nextPosition);
