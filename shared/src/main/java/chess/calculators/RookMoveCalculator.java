81aa88921f60f5efd3394c3a7afdd7aab7d05d64
Howon Jung <jhwlds@gmail.com>
2025-06-27 21:32:38 -0600

    passed rook move test

--- shared/src/main/java/chess/calculators/RookMoveCalculator.java
+++ shared/src/main/java/chess/calculators/RookMoveCalculator.java
@@ -1,7 +1,6 @@
 package chess.calculators;
 
 import chess.*;
-
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -15,27 +14,33 @@ public class RookMoveCalculator implements PieceMovesCalculator {
         ChessGame.TeamColor myColor = currentPiece.getTeamColor();
 
         int[][] directions = {
-                {-2, -1},
-                {-2, +1},
-                {-1, -2},
-                {-1, +2},
-                {+1, -2},
-                {+1, +2},
-                {+2, -1},
-                {+2, +1}
+                {-1, 0},
+                {+1, 0},
+                {0, -1},
+                {0, +1}
         };
 
         for (int[] i : directions) {
-            int row = position.getRow() + i[0];
-            int col = position.getColumn() + i[1];
+            int row = position.getRow();
+            int col = position.getColumn();
 
-            if (row < 1 || row > 8 || col < 1 || col > 8) continue;
+            while (true) {
+                row += i[0];
+                col += i[1];
 
-            ChessPosition nextPosition = new ChessPosition(row, col);
-            ChessPiece target = board.getPiece(nextPosition);
+                if (row < 1 || row > 8 || col < 1 || col > 8) break;
 
-            if (target == null || target.getTeamColor() != myColor) {
-                moves.add(new ChessMove(position, nextPosition, null));
+                ChessPosition nextPosition = new ChessPosition(row, col);
+                ChessPiece target = board.getPiece(nextPosition);
+
+                if (target == null) {
+                    moves.add(new ChessMove(position, nextPosition, null));
+                } else {
+                    if (target.getTeamColor() != myColor) {
+                        moves.add(new ChessMove(position, nextPosition, null));
+                    }
+                    break;
+                }
             }
         }
 
