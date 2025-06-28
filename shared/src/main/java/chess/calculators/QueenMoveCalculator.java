6d8cf847ce542404a7d2425c359e00198d714596
Howon Jung <jhwlds@gmail.com>
2025-06-27 21:36:32 -0600

    what a sweet combo, passed queen move test

--- shared/src/main/java/chess/calculators/QueenMoveCalculator.java
+++ shared/src/main/java/chess/calculators/QueenMoveCalculator.java
@@ -1,7 +1,6 @@
 package chess.calculators;
 
 import chess.*;
-
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -11,35 +10,10 @@ public class QueenMoveCalculator implements PieceMovesCalculator {
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
         Collection<ChessMove> moves = new ArrayList<>();
 
-        ChessPiece currentPiece = board.getPiece(position);
-        ChessGame.TeamColor myColor = currentPiece.getTeamColor();
-
-        int[][] directions = {
-                {-1, -1},
-                {-1, 0},
-                {-1, 1},
-                { 0, -1},
-                { 0, 1},
-                { 1, -1},
-                { 1, 0},
-                { 1, 1}
-        };
-
-        for (int[] i : directions) {
-            int row = position.getRow() + i[0];
-            int col = position.getColumn() + i[1];
-
-            if (row < 1 || row > 8 || col < 1 || col > 8)
-                continue;
-
-            ChessPosition nextPosition = new ChessPosition(row, col);
-            ChessPiece target = board.getPiece(nextPosition);
-
-            if (target == null || target.getTeamColor() != myColor) {
-                moves.add(new ChessMove(position, nextPosition, null));
-            }
-        }
+        moves.addAll(new RookMoveCalculator().pieceMoves(board, position));
+        moves.addAll(new BishopMoveCalculator().pieceMoves(board, position));
 
         return moves;
     }
 }
+
