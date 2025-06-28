575274552ad7a72c160364bb2e211a48b4860ba8
Howon Jung <jhwlds@gmail.com>
2025-06-27 20:55:17 -0600

    passing king move test

--- shared/src/main/java/chess/calculators/KingMoveCalculator.java
+++ shared/src/main/java/chess/calculators/KingMoveCalculator.java
@@ -1,7 +1,6 @@
 package chess.calculators;
 
 import chess.*;
-
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -12,37 +11,31 @@ public class KingMoveCalculator implements PieceMovesCalculator {
         Collection<ChessMove> moves = new ArrayList<>();
 
         ChessPiece currentPiece = board.getPiece(position);
-        ChessGame.TeamColor mycolor = currentPiece.getTeamColor();
+        ChessGame.TeamColor myColor = currentPiece.getTeamColor();
 
-        int [][] direction = {
+        int[][] directions = {
                 {-1, -1},
-                {-1, +1},
-                {+1, -1},
-                {+1, +1},
+                {-1, 0},
+                {-1, 1},
+                { 0, -1},
+                { 0, 1},
+                { 1, -1},
+                { 1, 0},
+                { 1, 1}
         };
 
-        for (int[] i : direction) {
-            int row = position.getRow();
-            int col = position.getColumn();
+        for (int[] i : directions) {
+            int row = position.getRow() + i[0];
+            int col = position.getColumn() + i[1];
 
-            while (true) {
-                row += i[0];
-                col += i[1];
+            if (row < 1 || row > 8 || col < 1 || col > 8)
+                continue;
 
-                if (row < 1 || row > 8 || col < 1 || col > 8)
-                    break;
+            ChessPosition nextPosition = new ChessPosition(row, col);
+            ChessPiece target = board.getPiece(nextPosition);
 
-                ChessPosition nextPosition = new ChessPosition(row, col);
-                ChessPiece target = board.getPiece(nextPosition);
-
-                if (target == null) {
-                    moves.add(new ChessMove(position, nextPosition,null));
-                } else {
-                    if (target.getTeamColor() != mycolor) {
-                        moves.add(new ChessMove(position, nextPosition, null));
-                    }
-                    break;
-                }
+            if (target == null || target.getTeamColor() != myColor) {
+                moves.add(new ChessMove(position, nextPosition, null));
             }
         }
 
