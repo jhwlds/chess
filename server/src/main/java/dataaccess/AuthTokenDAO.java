e117535efd5082c2c3a8b4f7bd101b801cc1767b
Howon Jung <jhwlds@gmail.com>
2025-07-12 19:47:30 -0600

    adding authtoken struc

--- server/src/main/java/dataaccess/AuthTokenDAO.java
+++ server/src/main/java/dataaccess/AuthTokenDAO.java
@@ -1,5 +1,10 @@
 package dataaccess;
 
+import model.AuthData;
+
 public interface AuthTokenDAO {
-    void clear();
+    void insertAuth(AuthData auth) throws DataAccessException;
+    AuthData getAuth(String token) throws DataAccessException;
+    void deleteAuth(String token) throws DataAccessException;
+    void clear() throws DataAccessException;
 }
--- server/src/main/java/dataaccess/MemoryAuthTokenDAO.java
+++ server/src/main/java/dataaccess/MemoryAuthTokenDAO.java
@@ -1,13 +1,30 @@
 package dataaccess;
 
+import model.AuthData;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class MemoryAuthTokenDAO implements AuthTokenDAO {
-    private final Map<String, String> tokens = new HashMap<>();
+    private final Map<String, AuthData> authTokens = new HashMap<>();
+
+    @Override
+    public void insertAuth(AuthData auth) throws DataAccessException {
+        authTokens.put(auth.authToken(), auth);
+    }
+
+    @Override
+    public AuthData getAuth(String token) throws DataAccessException {
+        return authTokens.get(token);
+    }
+
+    @Override
+    public void deleteAuth(String token) throws DataAccessException {
+        authTokens.remove(token);
+    }
 
     @Override
     public void clear() {
-        tokens.clear();
+        authTokens.clear();
     }
 }
