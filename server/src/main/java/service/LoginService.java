f5390e135a0260e338e34026a40f57be30b05860
Howon Jung <jhwlds@gmail.com>
2025-07-13 18:27:04 -0600

    bad request

--- server/src/main/java/service/LoginService.java
+++ server/src/main/java/service/LoginService.java
@@ -1,4 +1,24 @@
 package service;
 
+import dataaccess.AuthTokenDAO;
+import dataaccess.DAOFactory;
+import dataaccess.DataAccessException;
+import dataaccess.UserDAO;
+import shared.LoginRequest;
+import shared.LoginResult;
+
 public class LoginService {
+    private final UserDAO userDAO = DAOFactory.userDAO();
+    private final AuthTokenDAO authDAO = DAOFactory.authDAO();
+
+    public LoginResult login(LoginRequest req) {
+        try {
+            if (req.username() == null || req.password() == null) {
+                return new LoginResult("Error: bad request");
+            }
+
+        } catch (DataAccessException e) {
+            return new LoginResult("Error: " + e.getMessage());
+        }
+    }
 }
