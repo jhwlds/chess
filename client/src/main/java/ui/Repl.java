98088c8c14b37b5acba5e30f4e13d5a8ae11f553
Howon Jung <jhwlds@gmail.com>
2025-07-25 20:50:46 -0600

    started on getting input from user

--- client/src/main/java/ui/Repl.java
+++ client/src/main/java/ui/Repl.java
@@ -1,7 +1,43 @@
 package ui;
 
-public class Repl {
-    public Repl(int port) {}
+import client.ServerFacade;
+import java.util.Scanner;
 
-    public void run() {}
+public class Repl {
+    private final ServerFacade serverFacade;
+    private final Scanner scanner;
+    private String authToken;
+
+    public Repl(int port) {
+        this.serverFacade = new ServerFacade(port);
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void run() {
+        System.out.println("♕ 240 Chess Client");
+        System.out.println("Type 'help' for a list of commands");
+
+        while (true) {
+            if (authToken == null) {
+                preloginUI();
+            } else {
+                postloginUI();
+            }
+        }
+    }
+
+    private void preloginUI() {
+        System.out.print("\n[LOGGED_OUT] >>> ");
+        String input = scanner.nextLine().trim().toLowerCase();
+
+        switch (input) {
+            case "help":
+                showPreloginHelp();
+                break;
+        }
+    }
+
+    private void postloginUI() {}
+
+    private void showPreloginHelp() {}
 }
