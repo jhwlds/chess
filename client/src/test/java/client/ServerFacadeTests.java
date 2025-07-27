3af01e1f1b96a890adee854f1b7c060688511bc6
Howon Jung <jhwlds@gmail.com>
2025-07-27 00:11:32 -0600

    test

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -3,17 +3,29 @@ package client;
 import model.AuthData;
 import shared.CreateGameResult;
 import shared.GameListResult;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import server.Server;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ServerFacadeTests {
-    private ServerFacade facade;
-    private static final int PORT = 0;
+    private static Server server;
+    private static ServerFacade facade;
+
+    @BeforeAll
+    public static void init() {
+        server = new Server();
+        var port = server.run(0);
+        System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade(port);
+    }
+
+    @AfterAll
+    static void stopServer() {
+        server.stop();
+    }
 
     @BeforeEach
     void setUp() throws Exception {
-        facade = new ServerFacade(PORT);
         // Clear database before each test
         facade.clearDatabase();
     }
